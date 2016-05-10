6daaffb05c225dd9831ce75c5a7e195406ef2419
Suraj Arya <suraj.p.arya@gmail.com>
2016-05-10 19:25:30 +0530

    Fixed verfiy_otp and generate_otp and a new release on pypi

--- msg91/msg91.py
+++ msg91/msg91.py
@@ -14,16 +14,16 @@ class MSG91Exception(Exception):
 
 
 class MSG91Client(object):
-    """A simple API client for the msg91 & sendOTP APIs combines
+    """A simple API client for the msg91 & sendOTP APIs combined.
 
     It includes methods for calling sms api of msg91
-    and methods for calling sendOTP APIs
+    and methods for calling sendOTP APIs.
 
-    Both the API clients are combined as one API client
-     for the sake of brevity and simplicity
+    Both the API clients are combined as one for the sake of
+     brevity and simplicity.
 
     If you only want to send SMSs then you can just pass api key
-    at the time of instantiating and start making use of send sms methods
+    at the time of instantiating and start making use of send sms methods.
 
     In order to use otp APIs you will need to pass app_key  too.
     ref. https://github.com/SendOTP/SendOTPAndroid and
@@ -31,9 +31,6 @@ class MSG91Client(object):
     """
 
     def __init__(self, api_key, app_key=None, **kwargs):
-        """
-        """
-
         self.auth_key = api_key
         self.app_key = app_key
 
@@ -42,17 +39,19 @@ class MSG91Client(object):
 
         self.sms_url = 'https://control.msg91.com/api/sendhttp.php'
         self.otp_base_url = 'https://sendotp.msg91.com/api'
-        self.otp_ops = {'otp': 'generateOTP', 'status': 'checkStatus'}
+        self.otp_ops = {'otp': 'generateOTP',
+                        'status': 'checkStatus',
+                        'verify': 'verifyOTP'}
 
     def _get_otp_url(self, op):
         return '/'.join([self.otp_base_url, self.otp_ops[op]])
 
-    def _get_otp_headers(self):
+    def _get_app_key_headers(self):
         if self.app_key is None:
             raise MSG91Exception('App Key is not set.')
 
         return {'application-key': self.app_key,
-                'Content-Type': 'application/json'}
+                'content-type': 'application/json'}
 
     def generate_otp(self, mobile_number, country=91):
         url = self._get_otp_url('otp')
@@ -61,37 +60,52 @@ class MSG91Client(object):
                    'mobileNumber': str(mobile_number),
                    'getGeneratedOTP': True}
 
-        res = requests.post(url, json=json.dumps(payload),
-                            headers=self._get_otp_headers())
+        res = requests.post(url, data=json.dumps(payload),
+                            headers=self._get_app_key_headers())
+
+        return json.loads(res.content)
+
+    def verify_otp(self, mobile_number, otp, country=91):
+        url = self._get_otp_url('verify')
+        payload = {'countryCode': str(country),
+                   'mobileNumber': str(mobile_number),
+                   'oneTimePassword': str(otp)}
+
+        res = requests.post(url,
+                            data=json.dumps(payload),
+                            headers=self._get_app_key_headers())
 
         return json.loads(res.content)
 
     def check_otp_status(self, mobile, refresh_token, country=91):
-        res = requests.get(self._get_otp_url('status'), params={'mobileNumber': mobile,
-                                                                'countryCode': country,
-                                                                'refreshToken': refresh_token},
-                           headers=self._get_otp_headers())
+        res = requests.get(self._get_otp_url('status'),
+                           params={'mobileNumber': mobile,
+                                   'countryCode': country,
+                                   'refreshToken': refresh_token},
+                           headers=self._get_app_key_headers())
 
         return json.loads(res.content)
 
     def send_sms(self, message, mobile, country=91):
-        res = requests.get(self.sms_url, params={'authkey': self.auth_key,
-                                                 'mobiles': mobile,
-                                                 'message': message,
-                                                 'sender': self.sender_id,
-                                                 'route': self.route,
-                                                 'country': country,
-                                                 'response': 'json'})
+        res = requests.get(self.sms_url,
+                           params={'authkey': self.auth_key,
+                                   'mobiles': mobile,
+                                   'message': message,
+                                   'sender': self.sender_id,
+                                   'route': self.route,
+                                   'country': country,
+                                   'response': 'json'})
         return json.loads(res.content)
 
     def send_sms_bulk(self, message, country=91, *recipients):
         mobiles = ','.join(str(num) for num in recipients)
 
-        res = requests.get(self.sms_url, params={'authkey': self.auth_key,
-                                                 'mobiles': mobiles,
-                                                 'message': message,
-                                                 'sender': self.sender_id,
-                                                 'route': self.route,
-                                                 'country': country,
-                                                 'response': 'json'})
+        res = requests.get(self.sms_url,
+                           params={'authkey': self.auth_key,
+                                   'mobiles': mobiles,
+                                   'message': message,
+                                   'sender': self.sender_id,
+                                   'route': self.route,
+                                   'country': country,
+                                   'response': 'json'})
         return json.loads(res.content)
--- tests/test_msg91.py
+++ tests/test_msg91.py
@@ -16,23 +16,24 @@ from msg91 import MSG91Client
 class TestMsg91(unittest.TestCase):
 
     def setUp(self):
-        api_key = ''  # todo: add api key for testing sms
-        app_key = ''  # todo: add app key for testing otp
+        api_key = ''  # add your auth key for sending SMSs
+        app_key = ''  # add your Application Key for testing OTP APIs
         self.client = MSG91Client(api_key=api_key, app_key=app_key)
 
     def tearDown(self):
         pass
 
     def test_send_sms(self):
-        #self.client.send_sms('Test SMS', '9971636025')
-        pass
+        self.client.send_sms('Test SMS', '9971XXXXXX')
 
-    def test_verify_otp(self):
-        #self.client.check_otp_status('9971636025', 'adbkhDGvad72t8HJDVd396HD')
-        pass
+    def test_check_otp_status(self):
+        self.client.check_otp_status('9971XXXXXX', 'adbkhDGvad72t8HJDVd396HD')
 
     def test_send_otp(self):
         self.client.generate_otp('9971XXXXXX')
+
+    def test_verify_otp(self):
+        self.client.verify_otp('9971XXXXX', '6264')
 
 
 if __name__ == '__main__':
